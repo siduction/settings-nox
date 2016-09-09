8c2d69feeb4463fe5393e5f33aafcbc886eba8b4
Alf Gaida <agaida@siduction.org>
2016-09-09 21:01:28 +0200

    some changes

--- functions.sh
+++ functions.sh
@@ -56,7 +56,7 @@ function debian_changelog {
             -e "s/\@VERSION\@/${VERSION}/g" \
             -e "s/\@FLAVOUR\@/${FLAVOUR}/g" \
             -e "s/\@DISPLAY\@/${DISPLAY}/g" \
-            templates/debian/changelog \
+            ./debtemplate/changelog \
             > debian/changelog
     fi
 }
@@ -70,7 +70,7 @@ function basic_control {
         -e "s/\@VERSION\@/${VERSION}/g" \
         -e "s/\@FLAVOUR\@/${FLAVOUR}/g" \
         -e "s/\@DISPLAY\@/${DISPLAY}/g" \
-        templates/debian/${1}-control \
+        debtemplate/control \
         > debian/control
 }
 
@@ -78,14 +78,14 @@ function basic_control {
 # grub template
 # =============
 function grub_template {
-    mkdir -p ${1}/etc/default/grub.d
+    mkdir -p ./etc/default/grub.d
     sed -e "s/\@CODENAME_SAFE\@/${CODENAME_SAFE}/g" \
         -e "s/\@DISTRIBUTION\@/${DISTRIBUTION}/g" \
         -e "s/\@VERSION\@/${VERSION}/g" \
         -e "s/\@FLAVOUR\@/${1}/g" \
         -e "s/\@DISPLAY\@/${DISPLAY}/g" \
-        templates/grub/template.cfg \
-        > ${1}/etc/default/grub.d/siduction.cfg
+        ./template/template.cfg \
+        > etc/default/grub.d/siduction.cfg
 }
 
 
@@ -97,14 +97,16 @@ function debian_rules {
         -e "s/\@VERSION\@/${VERSION}/g" \
         -e "s/\@FLAVOUR\@/${FLAVOUR}/g" \
         -e "s/\@DISPLAY\@/${DISPLAY}/g" \
-        templates/debian/rules \
+        ./debtemplate/rules \
         > debian/rules
     chmod 755 debian/rules
 }
 
-# debian foo basics
-function debian_foo_basics {
-    cp -a templates/debian/source debian
-    cp    templates/debian/compat debian
-}
 
+# debian_foo_basics
+# =================
+function debian_foo_basics {
+    mkdir -p ./debian/source
+    echo 1.0 > ./debian/source/format
+    echo 9   > ./debian/compat
+}
